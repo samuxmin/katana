0291416717ac9187764f3fb0b8db070d19c55cd1
Samuel Mindler <mindlersamuel@gmail.com>
2023-01-06 12:11:15 -0300

    cambios para deploy del server

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import express from "express";
 import path from "path";
-const PORT = 3000;
+import cors from "cors"
+import dotenv from "dotenv"
+const PORT = process.env.PORT || "3000";
 const app = express()
 app.use(express.json())
+dotenv.config()
 app.use(express.static(path.join(path.resolve(),"public")))
+app.use(cors())
 app.get(["/", "/index.html"], (req, res) => {
     res.sendFile(path.resolve() + "/index.html");
 })
+
 app.listen(PORT)
